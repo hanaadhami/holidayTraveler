a3197a3944d71097dadb1fd98c878c56075c0d0e
AlvinDChipmunk <alvinrevilas@yahoo.com>
2020-01-09 20:53:47 -0800

    using sequelize passport files

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
+
+
+// Creating express app and configuring middleware needed for authentication
+var app = express();
+
 // Set Handlebars as the default templating engine. main is linking to the document under views. its boilerplate
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Creating express app and configuring middleware needed for authentication
-var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -29,7 +32,7 @@ require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-  });
+    app.listen(PORT, function() {
+        console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    });
 });
